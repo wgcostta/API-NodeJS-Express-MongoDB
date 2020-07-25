7637b7d1857f1adb6abe55d5ec0eaaae25cf3608
Wagner <wg.o.costa@gmail.com>
2020-07-25 14:40:32 -0300

    Autenticação e Nodemon

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const User = require("../model/User");
 const { Router } = require("express");
@@ -20,6 +21,20 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" + err });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(400).send({ error: "_User not found" });
+  }
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).send({ error: "Invalid _User or _Password" });
+
+  user.password = undefined;
+  res.send({ user });
 });
 
 module.exports = (app) => app.use("/auth", router);
