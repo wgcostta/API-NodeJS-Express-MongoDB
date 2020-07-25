3f84fb37ff527e020ee7d881471c847012967a78
Wagner <wg.o.costa@gmail.com>
2020-07-25 10:53:09 -0300

    Validando se o usuario já esta salvo

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -6,8 +6,16 @@ const { Router } = require("express");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
+
   try {
+    if (await User.findOne({ email }))
+      return res.status(400).send({ error: "_User already existis" });
+
     const user = await User.create(req.body);
+
+    user.password = undefined;
+
     return res.send({ user });
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" + err });
