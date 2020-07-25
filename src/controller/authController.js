f54ff2149ba905da964b3ea95f4c0972ff49de27
Wagner <wg.o.costa@gmail.com>
2020-07-25 14:46:28 -0300

    Gerando o token de Autenticação.

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const authConfig = require("../config/auth");
 
 const User = require("../model/User");
 const { Router } = require("express");
@@ -34,7 +37,12 @@ router.post("/authenticate", async (req, res) => {
     return res.status(400).send({ error: "Invalid _User or _Password" });
 
   user.password = undefined;
-  res.send({ user });
+
+  const token = jwt.sign({ id: user.id }, authConfig.secret, {
+    expiresIn: 86400,
+  });
+
+  res.send({ user, token });
 });
 
 module.exports = (app) => app.use("/auth", router);
