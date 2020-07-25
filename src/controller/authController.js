aada092771ded09828c792d0592f889a1f89d0b5
Wagner <wg.o.costa@gmail.com>
2020-07-25 16:18:35 -0300

    Ajustando rotinas de Tokens e criando rotina de Projetos

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -9,6 +9,12 @@ const { Router } = require("express");
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+  return jwt.sign({ params }, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
+
 router.post("/register", async (req, res) => {
   const { email } = req.body;
 
@@ -20,7 +26,7 @@ router.post("/register", async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" + err });
   }
@@ -38,11 +44,7 @@ router.post("/authenticate", async (req, res) => {
 
   user.password = undefined;
 
-  const token = jwt.sign({ id: user.id }, authConfig.secret, {
-    expiresIn: 86400,
-  });
-
-  res.send({ user, token });
+  res.send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = (app) => app.use("/auth", router);
--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./controller/authController")(app);
+require("./controller/projectController")(app);
 
 app.listen(3000);
