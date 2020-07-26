1285f050cac538ed768e0aab3282331c4974c96b
Wagner <wg.o.costa@gmail.com>
2020-07-25 21:13:29 -0300

    Alterando a estrutura do projeto
    Alterando para que todos os controllers sejam visiveis pelo index principal, através do index.js do controller em tempo de execução

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require("./controller/authController")(app);
-require("./controller/projectController")(app);
+require("./app/controller/index")(app);
 
 app.listen(3000);
