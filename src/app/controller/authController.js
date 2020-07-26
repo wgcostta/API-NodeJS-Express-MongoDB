5c5cc7b487e7df67f27cb60aab10222f56ffbd4e
Wagner <wg.o.costa@gmail.com>
2020-07-25 21:47:05 -0300

    Criando método para enviar um token de recuperação de senha

--- src/app/controller/authController.js
+++ src/app/controller/authController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const authConfig = require("../../config/auth");
+const mailer = require("../../modules/mailer");
 
 const User = require("../model/User");
 const { Router } = require("express");
@@ -45,6 +47,42 @@ router.post("/authenticate", async (req, res) => {
   user.password = undefined;
 
   res.send({ user, token: generateToken({ id: user.id }) });
+});
+
+router.post("/forgot_password", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send({ error: "_User not found" });
+    }
+
+    const token = crypto.randomBytes(20).toString("hex");
+
+    //Data e o tempo de expitação (1 hr a mais)
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await User.findByIdAndUpdate(user.id, {
+      $set: {
+        passwordResetToken: token,
+        passwordResetExpires: now,
+      },
+    });
+
+    console.log(token, now);
+    mailer.sendEmail({
+      to: email,
+      from: "wg.o.costa@gmail.com",
+      template: "",
+    });
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ error: "Erro on forgot password, try anain .." + err });
+  }
 });
 
 module.exports = (app) => app.use("/auth", router);
